32115ecde63c35ec8a6ce4efe29d8c615fa8bea7
WitchyFicha <caligoyan@gmail.com>
2020-08-28 17:59:16 +0600

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
-	    LexicalAnalyzer lex = new LexicalAnalyzer("src/input/case1.txt");
+	    LexicalAnalyzer lex = new LexicalAnalyzer("src/cases/case1.txt");
         try {
             lex.scanCode();
         } catch (IOException e) {
